610e94075f36848a8999b7a9461863e6a2fcc481
Jemil <oyebisijemil@gmail.com>
2021-06-14 18:34:34 -0400

    Got the hash value of a string with sha3

--- app.js
+++ app.js
@@ -5,3 +5,6 @@ const web3 = new Web3('HTTP://127.0.0.1:7545');
 web3.eth.getGasPrice().then((result) => {
    console.log(web3.utils.fromWei(result, 'ether'));
 })
+
+//get the hash value of a string with sha3
+console.log(web3.utils.sha3('Jemil'))
