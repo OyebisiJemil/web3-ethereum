77149bf016bb72c4a2086eb5678da0fc75b9aae8
Jemil <oyebisijemil@gmail.com>
2021-06-14 18:42:32 -0400

    Got the hash value of a string with soliditySha3, the same result with sha3 and keccak256 So web3 make available the solidity hash functionality

--- app.js
+++ app.js
@@ -9,5 +9,8 @@ web3.eth.getGasPrice().then((result) => {
 //get the hash value of a string with sha3
 console.log(web3.utils.sha3('Jemil'))
 
-//get the hash value of a string with sha3
+//get the hash value of a string with keccak256
 console.log(web3.utils.keccak256('Jemil'))
+
+//get the hash value of a string with soliditySha3
+console.log(web3.utils.soliditySha3('Jemil'))
