f4c8e2a63f51e98402155cfe5e99ed1f0e89e977
Jemil <oyebisijemil@gmail.com>
2021-06-14 18:52:22 -0400

    Tested out web3 support for underscore js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const Web3 = require('web3');
 const web3 = new Web3('HTTP://127.0.0.1:7545');
+const _ = web3.utils._
 
 //Calculate the gas price
 web3.eth.getGasPrice().then((result) => {
@@ -17,3 +18,9 @@ console.log(web3.utils.soliditySha3('Jemil'))
 
 //generate random hex
 console.log(web3.utils.randomHex(32))
+
+
+_.each({key1: 'value1', key2: 'value2'}, (value, key) => {
+    console.log(key);
+    console.log(value);
+})
