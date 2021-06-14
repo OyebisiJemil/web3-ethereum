ccfcacaa47ed1be9f4e280a2e7ba26a2d4497505
Jemil <oyebisijemil@gmail.com>
2021-06-14 18:28:36 -0400

    Added gitignore file, setup web3, and calculated gas price in ether

---
+++ app.js
@@ -0,0 +1,7 @@
+const Web3 = require('web3');
+const web3 = new Web3('HTTP://127.0.0.1:7545');
+
+//Calculate the gas price
+web3.eth.getGasPrice().then((result) => {
+   console.log(web3.utils.fromWei(result, 'ether'));
+})
